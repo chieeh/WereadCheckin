5f35213540e0cfbe1325e749b6fa7942c64d2fa9
chieeh <chieh@tutamail.com>
2026-03-26 12:09:50 +0800

    Enable AOT

--- app.cs
+++ app.cs
@@ -2,12 +2,11 @@
 #:package Microsoft.Extensions.DependencyInjection@*
 #:package Microsoft.Extensions.Http@*
 
-#:property PublishAot=false
-
-using System.Net;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using ConsoleAppFramework;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -70,17 +69,19 @@ public class Commands
         Account? account = null;
         if (configPath.StartsWith("http"))
         {
-            var result = await httpClientFactory.CreateClient().GetFromAsync<Account>(configPath);
-            if (!result.IsSuccessStatusCode)
-            {
-                Utils.Log("Failed to get account");
-                return 1;
-            }
-            account = result.Result;
+            using var client = httpClientFactory.CreateClient();
+            var rawContent = await client.GetStringAsync(configPath);
+            account = JsonSerializer.Deserialize(
+                rawContent,
+                SourceGenerationContext.Default.Account
+            );
         }
         else if (File.Exists(configPath))
         {
-            account = JsonSerializer.Deserialize<Account>(File.ReadAllText(configPath));
+            account = JsonSerializer.Deserialize(
+                File.ReadAllText(configPath),
+                SourceGenerationContext.Default.Account
+            );
         }
         if (account is null)
         {
@@ -90,92 +91,106 @@ public class Commands
         Utils.SensitiveData.Add(account.RefreshToken);
         Utils.SensitiveData.Add(account.DeviceId);
         Utils.SensitiveData.Add(account.Vid.ToString());
-        Utils.Log($"Account: {account.Vid}");
+        Utils.Log($"Account Vid: {account.Vid}");
         await Task.Delay(TimeSpan.FromMinutes(delay), stoppingToken);
         using var apiClient = httpClientFactory.CreateClient("api");
-        _ = await apiClient.GetFromAsync<object>("/");
-        var signatureResult = await apiClient.GetFromAsync<SignatureResponse>(
-            $"/generation/signature?deviceId={account.DeviceId}"
+        _ = await apiClient.GetAsync("/");
+        var signatureResult = await apiClient.GetFromJsonAsync<SignatureResponse>(
+            $"/generation/signature?deviceId={account.DeviceId}",
+            SourceGenerationContext.Default.SignatureResponse
         );
-        if (!signatureResult.IsSuccessStatusCode || signatureResult.Result is null)
+        if (!signatureResult.IsSuccessStatusCode || signatureResult.Value is null)
         {
             Utils.Log("Failed to get signature");
             return 1;
         }
 
         using var wereadClient = httpClientFactory.CreateClient("weread");
-        var loginResult = await wereadClient.PostFromAsync<LoginResponse>(
-            "/login",
-            new
-            {
-                deviceId = account.DeviceId,
-                deviceName = Device.Name,
-                inBackground = 0,
-                kickType = 1,
-                random = signatureResult.Result.Random,
-                refCgi = "",
-                refreshToken = account.RefreshToken,
-                signature = signatureResult.Result.Signature,
-                timestamp = signatureResult.Result.Timestamp,
-                trackId = "",
-                wxToken = 0,
-            }
+        var loginContent = new LoginRequest(
+            deviceId: account.DeviceId,
+            deviceName: Device.Name,
+            inBackground: 0,
+            kickType: 1,
+            random: signatureResult.Value.Random,
+            refCgi: "",
+            refreshToken: account.RefreshToken,
+            signature: signatureResult.Value.Signature,
+            timestamp: signatureResult.Value.Timestamp,
+            trackId: "",
+            wxToken: 0
         );
-        if (!loginResult.IsSuccessStatusCode || loginResult.Result?.accessToken is null)
+        var loginResult = await wereadClient.PostAsJsonAsync<LoginRequest, LoginResponse>(
+            "/login",
+            loginContent,
+            SourceGenerationContext.Default.LoginRequest,
+            SourceGenerationContext.Default.LoginResponse
+        );
+        if (!loginResult.IsSuccessStatusCode || loginResult.Value?.accessToken is null)
         {
             Utils.Log("Failed to login");
             return 1;
         }
-        wereadClient.DefaultRequestHeaders.Add("accesstoken", loginResult.Result.accessToken);
-        wereadClient.DefaultRequestHeaders.Add("vid", loginResult.Result.vid.ToString());
-        var tokenResult = await wereadClient.GetFromAsync<TokenResponse>("/config?token=1");
-        if (!tokenResult.IsSuccessStatusCode || tokenResult.Result?.token is null)
+        var loginResponse = loginResult.Value;
+        wereadClient.DefaultRequestHeaders.Add("accesstoken", loginResponse.accessToken);
+        wereadClient.DefaultRequestHeaders.Add("vid", loginResponse.vid.ToString());
+        var tokenResult = await wereadClient.GetFromJsonAsync<TokenResponse>(
+            "/config?token=1",
+            SourceGenerationContext.Default.TokenResponse
+        );
+        if (!tokenResult.IsSuccessStatusCode || tokenResult.Value?.token is null)
         {
             Utils.Log("Failed to get token");
             return 1;
         }
-        string token = tokenResult.Result.token;
+        string token = tokenResult.Value.token;
         Utils.SensitiveData.Add(token);
         Utils.Log($"Token: {token}");
 
-        var chapterInfosResult = await wereadClient.PostFromAsync<ChapterInfosResponse>(
+        var chapterInfosResult = await wereadClient.PostAsJsonAsync<
+            ChapterInfosRequest,
+            ChapterInfosResponse
+        >(
             "/book/chapterInfos",
-            new { bookIds = new string[] { bookId.ToString() }, synckeys = new int[] { 0 } }
+            new ChapterInfosRequest(
+                bookIds: new string[] { bookId.ToString() },
+                synckeys: new int[] { 0 }
+            ),
+            SourceGenerationContext.Default.ChapterInfosRequest,
+            SourceGenerationContext.Default.ChapterInfosResponse
         );
         if (
             !chapterInfosResult.IsSuccessStatusCode
-            || chapterInfosResult.Result?.data?[0].updated is null
+            || chapterInfosResult.Value?.data?[0].updated is null
         )
         {
             Utils.Log("Failed to get chapter infos");
             return 1;
         }
-        List<ChapterInfo> chapterInfos = chapterInfosResult.Result.data![0].updated!;
+        List<ChapterInfo> chapterInfos = chapterInfosResult.Value.data![0].updated!;
 
-        var getBookProgressResult = await wereadClient.GetFromAsync<GetBookProgressResponse>(
-            $"/book/getProgress?bookId={bookId}"
+        var getBookProgressResult = await wereadClient.GetFromJsonAsync<GetBookProgressResponse>(
+            $"/book/getProgress?bookId={bookId}",
+            SourceGenerationContext.Default.GetBookProgressResponse
         );
-        if (
-            !getBookProgressResult.IsSuccessStatusCode || getBookProgressResult.Result?.book is null
-        )
+        if (!getBookProgressResult.IsSuccessStatusCode || getBookProgressResult.Value?.book is null)
         {
             Utils.Log("Failed to get book progress");
             return 1;
         }
-        var bookProgress = getBookProgressResult.Result.book;
+        var bookProgress = getBookProgressResult.Value.book;
         int readWord = readTime * speed;
         int chapterOffset = bookProgress.chapterOffset + readWord;
         int progress = bookProgress.progress;
 
-        int charpterIdx = bookProgress.chapterIdx;
-        if (charpterIdx == 0)
+        int chapterIdx = bookProgress.chapterIdx;
+        if (chapterIdx == 0)
         {
-            charpterIdx = chapterInfos.First().chapterIdx;
+            chapterIdx = chapterInfos.First().chapterIdx;
         }
-        int chapterUid = bookProgress.charpterUid;
-        for (int i = charpterIdx - 1; i < chapterInfos.Count; i = (i + 1) % chapterInfos.Count)
+        int chapterUid = bookProgress.chapterUid;
+        for (int i = chapterIdx - 1; i < chapterInfos.Count; i = (i + 1) % chapterInfos.Count)
         {
-            charpterIdx = chapterInfos[i].chapterIdx;
+            chapterIdx = chapterInfos[i].chapterIdx;
             chapterUid = chapterInfos[i].chapterUid;
             if (chapterOffset < chapterInfos[i].wordCount)
             {
@@ -189,7 +204,7 @@ public class Commands
             appId: account.DeviceId,
             bookId: bookId.ToString(),
             bookVersion: bookProgress.bookVersion,
-            charpterIdx: charpterIdx,
+            chapterIdx: chapterIdx,
             chapterOffset: chapterOffset,
             chapterUid: chapterUid,
             progress: progress,
@@ -198,24 +213,30 @@ public class Commands
             summary: bookProgress.summary,
             synckey: bookProgress.synckey
         );
-        signatureResult = await apiClient.GetFromAsync<SignatureResponse>(
-            $"/generation/signature?token={token}"
+        signatureResult = await apiClient.GetFromJsonAsync<SignatureResponse>(
+            $"/generation/signature?token={token}",
+            SourceGenerationContext.Default.SignatureResponse
         );
-        if (!signatureResult.IsSuccessStatusCode || signatureResult.Result is null)
+        if (!signatureResult.IsSuccessStatusCode || signatureResult.Value is null)
         {
             Utils.Log("Failed to get signature");
             return 1;
         }
-        var response = await wereadClient.PostFromAsync<SimpleResponse>(
+        var response = await wereadClient.PostAsJsonAsync<
+            UploadBookProgressRequest,
+            SimpleResponse
+        >(
             "/book/batchUploadProgress",
             new UploadBookProgressRequest(
                 books: [bookProgressInfo],
-                random: signatureResult.Result.Random,
-                signature: signatureResult.Result.Signature,
-                timestamp: signatureResult.Result.Timestamp
-            )
+                random: signatureResult.Value.Random,
+                signature: signatureResult.Value.Signature,
+                timestamp: signatureResult.Value.Timestamp
+            ),
+            SourceGenerationContext.Default.UploadBookProgressRequest,
+            SourceGenerationContext.Default.SimpleResponse
         );
-        if (!response.IsSuccessStatusCode || response.Result?.succ != 1)
+        if (!response.IsSuccessStatusCode || response.Value?.succ != 1)
         {
             Utils.Log("Failed to update book progress");
             return 1;
@@ -226,13 +247,23 @@ public class Commands
 }
 
 #region Models
-public class ClientResult<T>
-{
-    public bool IsSuccessStatusCode { get; init; }
-    public HttpStatusCode StatusCode { get; init; }
-    public HttpResponseHeaders? Headers { get; init; }
-    public T? Result { get; set; }
-}
+[JsonSerializable(typeof(Account))]
+[JsonSerializable(typeof(SimpleResponse))]
+[JsonSerializable(typeof(SignatureResponse))]
+[JsonSerializable(typeof(LoginResponse))]
+[JsonSerializable(typeof(LoginRequest))]
+[JsonSerializable(typeof(TokenResponse))]
+[JsonSerializable(typeof(BookInfo))]
+[JsonSerializable(typeof(ChapterInfo))]
+[JsonSerializable(typeof(ChapterInfosData))]
+[JsonSerializable(typeof(ChapterInfosResponse))]
+[JsonSerializable(typeof(ChapterInfosRequest))]
+[JsonSerializable(typeof(BookProgressInfoResponse))]
+[JsonSerializable(typeof(GetBookProgressResponse))]
+[JsonSerializable(typeof(BookProgressInfo))]
+[JsonSerializable(typeof(UploadBookProgressRequest))]
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Default)]
+internal partial class SourceGenerationContext : JsonSerializerContext { }
 
 public record Account(int Vid, string RefreshToken, string DeviceId);
 
@@ -241,6 +272,20 @@ public record SimpleResponse(int succ);
 public record SignatureResponse(string DeviceId, long Timestamp, int Random, string Signature);
 
 public record LoginResponse(int vid, string accessToken, string? refreshToken);
+
+public record LoginRequest(
+    string deviceId,
+    string deviceName,
+    int inBackground,
+    int kickType,
+    int random,
+    string refCgi,
+    string refreshToken,
+    string signature,
+    long timestamp,
+    string trackId,
+    int wxToken
+);
 
 public record TokenResponse(string token, long timestamp);
 
@@ -252,11 +297,13 @@ public record ChapterInfosData(BookInfo book, List<ChapterInfo> updated);
 
 internal record ChapterInfosResponse(List<ChapterInfosData> data);
 
+public record ChapterInfosRequest(string[] bookIds, int[] synckeys);
+
 public record BookProgressInfoResponse(
     string appId,
     int bookVersion,
     int chapterIdx,
-    int charpterUid,
+    int chapterUid,
     int chapterOffset,
     string summary,
     int readingTime,
@@ -270,7 +317,7 @@ public record BookProgressInfo(
     string appId,
     string bookId,
     long bookVersion,
-    int charpterIdx,
+    int chapterIdx,
     int chapterOffset,
     int chapterUid,
     int readingTime,
@@ -288,47 +335,72 @@ public record UploadBookProgressRequest(
 );
 
 #endregion
+
 public static class HttpClientExtensions
 {
-    public static async Task<ClientResult<T>> GetFromAsync<T>(this HttpClient client, string? uri)
+    public static async Task<HttpResponseMessageWrapper<TResponse>> GetFromJsonAsync<TResponse>(
+        this HttpClient client,
+        string? uri,
+        JsonTypeInfo<TResponse> jsonTypeInfo
+    )
     {
         var response = await client.GetAsync(uri);
         Utils.Log($"GET {response.RequestMessage?.RequestUri} {response.StatusCode}");
-        var result = new ClientResult<T>()
-        {
-            IsSuccessStatusCode = response.IsSuccessStatusCode,
-            StatusCode = response.StatusCode,
-            Headers = response.Headers,
-        };
+        TResponse? result = default;
         if (response.IsSuccessStatusCode)
         {
-            result.Result = await response.Content.ReadFromJsonAsync<T>();
+            result = await response.Content.ReadFromJsonAsync(jsonTypeInfo);
         }
-
-        return result;
+        return new HttpResponseMessageWrapper<TResponse>(response, result);
     }
 
-    public static async Task<ClientResult<T>> PostFromAsync<T>(
+    public static async Task<HttpResponseMessageWrapper<TResponse>> PostAsJsonAsync<
+        TRequest,
+        TResponse
+    >(
         this HttpClient client,
         string? uri,
-        object? content
+        TRequest content,
+        JsonTypeInfo<TRequest> requestTypeInfo,
+        JsonTypeInfo<TResponse> responseTypeInfo
     )
     {
-        var response = await client.PostAsJsonAsync(uri, content);
-        Utils.Log($"POST {response.RequestMessage?.RequestUri} {response.StatusCode}");
-        var result = new ClientResult<T>()
+        using var stream = new MemoryStream();
+        var writer = new Utf8JsonWriter(
+            stream,
+            new JsonWriterOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping }
+        );
+        await using (writer)
         {
-            IsSuccessStatusCode = response.IsSuccessStatusCode,
-            StatusCode = response.StatusCode,
-            Headers = response.Headers,
-        };
-
+            JsonSerializer.Serialize(writer, content, requestTypeInfo);
+        }
+        stream.Position = 0;
+        var httpContent = new StreamContent(stream);
+        httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(
+            "application/json"
+        );
+        var response = await client.PostAsync(uri, httpContent);
+        Utils.Log($"POST {response.RequestMessage?.RequestUri} {response.StatusCode}");
+        TResponse? result = default;
         if (response.IsSuccessStatusCode)
         {
-            result.Result = await response.Content.ReadFromJsonAsync<T>();
+            result = await response.Content.ReadFromJsonAsync(responseTypeInfo);
         }
+        return new HttpResponseMessageWrapper<TResponse>(response, result);
+    }
+}
 
-        return result;
+public class HttpResponseMessageWrapper<T> : HttpResponseMessage
+{
+    public T? Value { get; }
+
+    public HttpResponseMessageWrapper(HttpResponseMessage response, T? value)
+    {
+        StatusCode = response.StatusCode;
+        ReasonPhrase = response.ReasonPhrase;
+        Version = response.Version;
+        Content = response.Content;
+        Value = value;
     }
 }
 
